e0e9d853a1e684424fac48b80629dec034c10f61
Arun Bharadwaj <Arun-UB@users.noreply.github.com>
2016-06-04 20:03:31 -0400

    Made widgets sortable

---
+++ public/assignment/directives/jga-directives.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict";
+    angular
+        .module("WebAppMaker")
+        .directive("draggable", draggable);
+
+    function draggable() {
+        return {
+            restrict: "A",
+            link: function (scope, elem, attr) {
+                console.log(elem);
+                console.log(attr);
+                $(elem).sortable(
+                    {
+                        axis: "y",
+                        containment: elem,
+                        handle: ".glyphicon-align-justify",
+                        cursor: "move",
+                        // delay: 100, // To prevent unwanted drags while clicking
+                        opacity: 0.5
+                    });
+
+            }
+        }
+    }
+})($);
