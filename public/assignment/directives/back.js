50f3e06d571a0845e48a4d8e753cdd930d9db58e
Arun Bharadwaj <Arun-UB@users.noreply.github.com>
2016-06-05 22:20:20 -0400

    Refractoring

---
+++ public/assignment/directives/back.js
@@ -0,0 +1,18 @@
+(function () {
+    "use strict";
+    angular
+        .module("WebAppMaker")
+        .directive('back', back);
+
+    function back($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                elem.bind('click', function () {
+                    $window.history.back();
+                });
+            }
+        };
+    };
+
+})();
