970231f261cc5e219878aac491d147965b41d542
Arun Bharadwaj <Arun-UB@users.noreply.github.com>
2016-06-12 21:28:52 -0400

    Added mongoose config vars

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,15 @@
 module.exports = function () {
     'use strict';
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://admin:GW8XWk6vbhEZ@localhost/webdev');
+    var connectionString = 'mongodb://admin:GW8XWk6vbhEZ@localhost/webdev';
+    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(connectionString);
 
     var models = {
         userModel: require('./user/user.model.server')(),
